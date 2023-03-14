ff37c6de308df8410437e8697200cd5dc953021a
Klimkin <bluemars@yandex.ru>
2023-03-14 13:33:54 +0300

    Добавлен конфиг для docker

--- client/src/http.ts
+++ client/src/http.ts
@@ -5,10 +5,9 @@ import { logout } from './services/user';
 import { LOCAL_STORAGE_TOKEN } from './entities/User/constants';
 
 const UNAUTH_ERROR = 401;
-const API_URL = 'http://localhost:7000/api';
 
 const $api = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const interceptor = ({ dispatch }: typeof store) => {
